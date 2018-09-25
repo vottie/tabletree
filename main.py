e7c2eef3c8e7e59d87aa6d7a5fdbcaed14a4b7f4
vottie <tsuboi23@gmail.com>
2018-09-25 22:55:48 +0900

    Add set parent method.

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from leaf import Leaf
 if __name__ == '__main__':
     """ prepare """ 
     print("start")
-    parent = Node("root")
+    root = Node("root")
     leaf1 = Leaf("1-1")
-    parent.add(leaf1)
+    leaf1.set_parent(root)
     leaf2 = Leaf("1-2")
-    parent.add(leaf2)
+    leaf2.set_parent(root)
     node2 = Node("node2")
-    parent.add(node2)
+    node2.set_parent(root)
+    node3 = Node("node3")
+    node3.set_parent(node2)
+
+    root.add(leaf1)
+    root.add(leaf2)
+    root.add(node2)
+
+    node2.add(node3)
 
     """ execute """ 
-    parent.show_all()
+    root.show_all()
+    node2.show_all()
--- node.py
+++ node.py
@@ -10,10 +10,14 @@ class Node():
         self.children.append(node)
         node.parent = self
 
+    def set_parent(self, parent):
+        self.parent = parent
+
     def show_all(self):
         for i in self.children:
             print("node name is...")
             i.show()
 
     def show(self):
-        print("name = {0}.".format(self.name))
+        print("name   = {0}.".format(self.name))
+        print("parent = {0}.".format(self.parent.name))
